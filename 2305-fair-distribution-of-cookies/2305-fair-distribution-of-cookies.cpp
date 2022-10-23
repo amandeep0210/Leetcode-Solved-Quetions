f1c5ed60ebd9e2edbf4ef4a2aee0b4237b35631a
Amandeep <76957483+niraniaaman@users.noreply.github.com>
2022-10-23 12:14:18 +0530

    Time: 1814 ms (32.04%), Space: 6.8 MB (96.99%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -3,10 +3,12 @@ public:
     int result = INT_MAX;
     void solve(int ind, vector<int>&ans, vector<int> &cookies){
         if(ind >= cookies.size()){
+        // find the maximum element that is the bag sum
            int k = *max_element(ans.begin(), ans.end());
            result = min(result, k);
             return;
         }
+        // a particular cook can go to the each bag out of k bags
         for(int i = 0; i<ans.size(); i++){
             ans[i] += cookies[ind];
             solve(ind+1, ans, cookies);
