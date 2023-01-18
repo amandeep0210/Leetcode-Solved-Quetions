8c4b9368a0b5397d2918a408c0252d9f8b12678b
Amandeep <76957483+niraniaaman@users.noreply.github.com>
2023-01-18 23:02:24 +0530

    Added solution - LeetHub

---
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+      int r = 0;
+      int l = N-1;
+      while(r<=l){
+          int m = r +(l-r)/2;
+          if(arr[m] == K)return 1;
+          else if(arr[m] < K){
+              r = m+1;
+          }
+          else{
+              l = m-1;
+          }
+      }
+      return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+
+// } Driver Code Ends
