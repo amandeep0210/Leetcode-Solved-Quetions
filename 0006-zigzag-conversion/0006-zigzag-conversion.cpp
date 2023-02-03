c45c2c35d54e5b67880cdeb2daecc2596f87b8e9
Amandeep <76957483+niraniaaman@users.noreply.github.com>
2023-02-03 14:30:05 +0530

    Time: 170 ms (5.44%), Space: 40 MB (10.88%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+       int r = numRows;
+        if(r == 1)return s;
+       int c = s.length();
+       char k = '1';
+       vector<vector<char>> mat(r, vector<char>(c, k));
+        // for(int i = 0; i< r; i++){
+        //     for(int j = 0; j< c; j++){
+        //         cout << mat[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        int k1 = 0;
+        int k2 = 0;
+        bool f = true;
+        int i = 0;
+      while(i<s.length()){
+          if(k1 == 0){
+              mat[k1][k2] = s[i++];
+              k1++;
+              f = true;
+          }
+          else if(k1 == r-1){
+              mat[k1][k2] = s[i++];
+              f =  false;
+              k1--;
+              k2++;
+          }
+          else{
+              if(f == true){
+                  mat[k1][k2] = s[i++];
+                  k1++;
+              }
+              else{
+                  mat[k1][k2] = s[i++];
+                  k1--;
+                  k2++;
+              }
+          }
+      }
+      string res = "";
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
+                if(mat[i][j] != '1')res.push_back(mat[i][j]);
+            }
+        }
+       
+       return res;
+        
+        
+    }
+};
