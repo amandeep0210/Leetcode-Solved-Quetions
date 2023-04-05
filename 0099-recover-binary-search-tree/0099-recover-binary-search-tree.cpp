993acd8f66832e29ee82bbe43350d23f528ccf8e
Amandeep <76957483+amandeep0210@users.noreply.github.com>
2023-04-05 12:53:49 +0530

    Time: 45 ms (20.96%), Space: 57.9 MB (75.83%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,38 +11,25 @@
  */
 class Solution {
 public:
-    vector<int>v1;
+    TreeNode* temp1 = NULL, *temp2 = NULL, *prev = NULL;
     void inorder(TreeNode* root){
-        if(!root)return ;
-        inorder(root->left);
-        v1.emplace_back(root->val);
-        inorder(root->right);
-    }
-    void Inorder(TreeNode* root, int a1, int a2){
         if(!root)return;
-        Inorder(root->left, a1, a2);
-        if(root->val == a1)root->val = a2;
-        else if(root->val == a2)root->val = a1;
-        Inorder(root->right, a1, a2);
-    }
-    void recoverTree(TreeNode* root) {
-        inorder(root);
-        vector<int>v2(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
-        int a1, a2;
-        bool f = false;
-        for(int i = 0; i<v2.size(); i++){
-            if(v1[i] != v2[i]){
-                if(f == false){
-                a1 = v1[i];
-                 f = true;
-                }
-                else{
-                a2 = v1[i];
-                break;
-                }
+        inorder(root->left);
+        if(prev != NULL and prev->val > root->val){
+            if(temp1 == NULL){
+                temp1 = prev;
+                temp2 = root;
+            }
+            else{
+                temp2 = root;
             }
         }
-        Inorder(root, a1, a2);
+        prev = root;
+        inorder(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        if(!root)return;
+        inorder(root);
+        swap(temp1->val, temp2->val);
     }
 };
